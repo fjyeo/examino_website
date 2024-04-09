f8f6201b2d9ddeddaf69941425215072adbc82be
Freddie Yeo <63839465+fjyeo@users.noreply.github.com>
2024-04-09 17:41:08 +0100

    first upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    });
+
+    document.querySelectorAll('.feature-item, .contact').forEach((section) => {
+        observer.observe(section);
+    });
+});
